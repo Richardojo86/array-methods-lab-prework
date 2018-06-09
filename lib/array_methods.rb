f68e0ba0dec728dd118fb210884e821ec4c99106
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-09 00:41:59 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,27 +1,27 @@
 def using_include(array, element)
-  if arr.include?(element)
+  if array.include?(element)
   return true 
-else 
+  else 
   return false 
-end 
+  end 
 end
 
 def using_sort(array)
-
+array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+array.first
 end
 
 def using_last(array)
-
+array.last 
 end
 
 def using_size(array)
-
+array.size 
 end
